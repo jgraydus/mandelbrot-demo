694a10ef8d99315715eaa8e3fab4c0eb1073eed7
josh <jgraydus@gmail.com>
2024-07-04 13:29:43 -0600

    had to fix stuff that wasn't broken because javascript

--- webpack.config.js
+++ webpack.config.js
@@ -5,24 +5,24 @@ const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 const dist = path.resolve(__dirname, "dist");
 
 module.exports = {
-  mode: "production",
-  entry: {
-    index: "./js/index.js"
-  },
-  output: {
-    path: dist,
-    filename: "[name].js"
-  },
-  devServer: {
-    contentBase: dist,
-  },
-  plugins: [
-    new CopyPlugin([
-      path.resolve(__dirname, "static")
-    ]),
+    mode: "production",
+    entry: { index: "./js/index.js" },
+    experiments: { asyncWebAssembly: true },
+    output: {
+    	path: dist,
+    	filename: "[name].js"
+    },
+    devServer: { contentBase: dist },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "static"), 
+                    to: path.resolve(__dirname, "dist")
+                },
+            ]
+        }),
+        new WasmPackPlugin({ crateDirectory: __dirname })
+    ]
+}
 
-    new WasmPackPlugin({
-      crateDirectory: __dirname,
-    }),
-  ]
-};
